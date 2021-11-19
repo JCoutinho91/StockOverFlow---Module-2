ba7bdfefe7f23d22054d86f7ab67b7e548ab5a69
JosePedro <guedes.jose@gmail.com>
2021-11-19 15:12:56 +0000

    updatedauthroutes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,7 @@
 const router = require("express").Router();
 const User = require("./../models/User.model");
-const bcrypt = require("bcryptjs");
-const zxcvbn = require("zxcvbn");
-const isLoggedIn = require("./../middleware/isLoggedIn");
 //Importing Needed Packages
 
-
-const saltRounds = 10;
 
 
 
