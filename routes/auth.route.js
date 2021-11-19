a3a97c4d14ef3c314fcd34fd9561675c634b4b14
JosePedro <guedes.jose@gmail.com>
2021-11-19 16:22:20 +0000

    startedloginroutes

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,18 +2,21 @@ const router = require("express").Router();
 const User = require("./../models/User.model");
 //Importing Needed Packages
 
+const saltRounds = 10;
 
-
-
-
-
-
-
-
-
-
-
-
+router.get("/signup", (req, res) => {
+  res.render("auth-views/signup-form");
+});
+router.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  const usernameNotProvided = !username || username === "";
+  const passwordNotProvided = !password || password === "";
+  if (usernameNotProvided || passwordNotProvided) {
+    res.render("auth-views/signup-form", {
+      errorMessage: "Provide username and password.",
+    });
+    return;}
+})
 
 
 module.exports = router;
