882d9e7c2445328bab0ab364576a04ac75ae960d
JosePedro <guedes.jose@gmail.com>
2021-11-20 17:41:54 +0000

    dataimporteded and basic display

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,4 +1,19 @@
 const router = require("express").Router();
+const axios = require("axios");
+const url = "https://www.styvio.com/apiV2/AAPL/6884aa98-de8d-4ae2-bcd1-bab479e90c8b"
+
+router.get("/stocks-views/stock-view", (req,res)=>{
+    axios.get(url)
+    .then((stockdata) =>{
+        console.log(stockdata.data.ticker)
+        res.render("home-view", { stockList: stockdata.data });
+    })
+
+    .catch((err)=>{
+        console.log(err)
+    })
+
+})
 
 
 
