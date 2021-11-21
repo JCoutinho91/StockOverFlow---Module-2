6ae91b38c5b1909525c31cead865f77ca25b15f1
JosePedro <guedes.jose@gmail.com>
2021-11-21 16:31:19 +0000

    added .envprocess

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const axios = require("axios");
-const url = "https://www.styvio.com/apiV2/AAPL/6884aa98-de8d-4ae2-bcd1-bab479e90c8b"
+const url = "https://www.styvio.com/apiV2/AAPL/process.env.API_KEY"
 
 router.get("/stocks-views/stock-view", (req,res)=>{
     axios.get(url)
