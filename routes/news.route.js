96a9a94cf381a733b81f582d32e21fbd0b88e0ce
JosePedro <guedes.jose@gmail.com>
2021-11-21 15:44:09 +0000

    resolvingconflit

--- routes/news.route.js
+++ routes/news.route.js
@@ -5,15 +5,10 @@ router.get("/newsfeed", (req,res)=>{
     res.render("news-view")
 })
 
+
 router.get("/news-details", (req,res)=>{
     res.render("news-view-detail")
 })
-
-
-
-
-
-
 
 
  module.exports = router;
