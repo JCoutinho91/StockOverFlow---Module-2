3d5ea44d13774aa53189224d8df6f4a456872da5
JosePedro <guedes.jose@gmail.com>
2021-11-22 12:48:34 +0000

    apikeyonenv

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,20 +1,22 @@
 const router = require("express").Router();
 const axios = require("axios");
-
-
+require("dotenv").config();
+const urlApple = `https://www.styvio.com/apiV2/AAPL/${process.env.API_KEY}`
+const urlAmazon = `https://www.styvio.com/apiV2/AMZN/${process.env.API_KEY}`
+const urlTesla = `https://www.styvio.com/apiV2/TESL/${process.env.API_KEY}`
+const urlMicrosoft = `https://www.styvio.com/apiV2/MSFT/${process.env.API_KEY}`
 router.get("/stocks-views/stock-view", (req,res)=>{
      res.render("home-view")
 
     })
 
-router.get("/stockcategory" , (req,res)=>{
-   const pr1 = axios.get(urlApple)
-   const pr2 = axios.get(urlAmazon)
-   const pr3 = axios.get(urlTesla)
-   const pr4 = axios.get(urlMicrosoft)
-   Promise.all([pr1, pr2, pr3, pr4])
+router.get("/stockcategory", (req,res)=>{
+  const arrayStocks = ["AAPL","AMZN","TESL","MSFT","AA"]
+ const stocksPrs =  arrayStocks.map((ticker)=>{
+    return axios.get(`https://www.styvio.com/apiV2/${ticker}/${process.env.API_KEY}`)
+  })
+   Promise.all(stocksPrs)
    .then((values) => {
-    console.log("values1234:",values)
     res.render("./stocks-views/stock-view", {stockList : values})
   });
 })
