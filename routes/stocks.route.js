be5490c2f44e780f5ca2c21abe0374dafa4aff8c
JosePedro <guedes.jose@gmail.com>
2021-11-22 10:17:05 +0000

    trying promise.all2

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const axios = require("axios");
-const urlApple = "https://www.styvio.com/apiV2/AAPL/ac994d76-f036-47ba-a680-70d6401a6709"
-const urlAmazon = "https://www.styvio.com/apiV2/TSLA/ac994d76-f036-47ba-a680-70d6401a6709"
-const urlTesla = "https://www.styvio.com/apiV2/AMZN/ac994d76-f036-47ba-a680-70d6401a6709"
-const urlMicrosoft = "https://www.styvio.com/apiV2/MSFT/ac994d76-f036-47ba-a680-70d6401a6709"
-const urlGoogle = "https://www.styvio.com/apiV2/GOGGL/ac994d76-f036-47ba-a680-70d6401a6709"
+
 
 router.get("/stocks-views/stock-view", (req,res)=>{
      res.render("home-view")
