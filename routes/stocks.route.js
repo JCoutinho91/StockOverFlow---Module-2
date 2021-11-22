742e8bd6c5919c0b5a2ca6e33e1a69a6684567af
JosePedro <guedes.jose@gmail.com>
2021-11-22 21:49:12 +0000

    commentary working

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -18,31 +18,29 @@ router.get("/stockcategory", (req, res) => {
     res.render("./stocks-views/stock-view", { stockList: values });
   });
 });
-router.get("/stock-view-details/:stockId", (req, res) => {
-  const stockId = req.params.stockId;
-  return axios
-    .get(`https://www.styvio.com/apiV2/${stockId}/${process.env.API_KEY}`)
-    .then((stock) => {
-      res.render("./stocks-views/stock-view-details", { stockInfo: stock });
-      Comment.find({ ticker: stockId });
-    });
-  Comment.find({ ticker: stockId }).then((foundComment) => {
-    console.log(foundComment);
-    res.render("./stocks-views/stock-view-details", { foundComment });
-  });
-});
-
-router.post("/stock-view-details/:stockId/create", (req,res)=>{
+router.get("/stock-view-details/:stockId", async (req,res)=>{
   const stockId = req.params.stockId
-  const { name, comment } = req.body;
   try{
   const gettingData = await axios.get(`https://www.styvio.com/apiV2/${stockId}/${process.env.API_KEY}`)
-  const filteredComment = await Comment.create({ name: name , comment: comment , ticker : stockId})
-  res.redirect(`stock-view-details`)
+  const filteredComment = await Comment.find({ticker: stockId})
+    res.render("./stocks-views/stock-view-details", {data: {
+      stockInfo: gettingData,
+      foundComment: filteredComment}})
   }
-catch(err){
-  console.log(err)
-}})
+    catch(err){console.log(err)}
+})
+
+router.post("/stock-view-details/:stockID/create", (req,res)=>{
+  console.log("in post")
+  const stockId = req.params.stockID
+  const { name, comment } = req.body;
+  Comment.create({ name: name , comment: comment , ticker : stockId})
+  .then((createdComment)=>{
+    console.log(createdComment)
+  })
+  res.redirect(`/stock-view-details/${stockId}`)
+  })
+
 
 
 
