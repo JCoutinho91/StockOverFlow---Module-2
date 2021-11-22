5d6410e63bd1c9101f6c79a7af5135c2e9214b98
Max Mayer <mxhmayer@gmail.com>
2021-11-22 21:34:25 +0000

    add /routes/index.js and news.routes.js

--- routes/news.route.js
+++ routes/news.route.js
@@ -1,14 +1,12 @@
- const router = require("express").Router();
+const router = require("express").Router();
+const axios = require("axios");
 
+router.get("/newsfeed", (req, res) => {
+  res.render("news-view");
+});
 
-router.get("/newsfeed", (req,res)=>{
-    res.render("news-view")
-})
+router.get("/news-details", (req, res) => {
+  res.render("news-view-detail");
+});
 
-
-router.get("/news-details", (req,res)=>{
-    res.render("news-view-detail")
-})
-
-
- module.exports = router;
+module.exports = router;
