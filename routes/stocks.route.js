eb671082417ff94ca6f9760d6c54d5224a775aae
JosePedro <guedes.jose@gmail.com>
2021-11-22 11:53:35 +0000

    fixedpromisealldisplay

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const axios = require("axios");
+const urlApple = "https://www.styvio.com/apiV2/AAPL/70423544-06fc-489c-82e5-3508e4201962"
+const urlAmazon = "https://www.styvio.com/apiV2/TSLA/70423544-06fc-489c-82e5-3508e4201962"
+const urlTesla = "https://www.styvio.com/apiV2/AMZN/70423544-06fc-489c-82e5-3508e4201962"
+const urlMicrosoft = "https://www.styvio.com/apiV2/MSFT/70423544-06fc-489c-82e5-3508e4201962"
+const urlGoogle = "https://www.styvio.com/apiV2/GOGGL/70423544-06fc-489c-82e5-3508e4201962"
 
 
 router.get("/stocks-views/stock-view", (req,res)=>{
@@ -14,7 +19,8 @@ router.get("/stockcategory" , (req,res)=>{
    const pr4 = axios.get(urlMicrosoft)
    Promise.all([pr1, pr2, pr3, pr4])
    .then((values) => {
-    console.log("values1234:", values[3])
+    console.log("values1234:",values)
+    res.render("./stocks-views/stock-view", {stockList : values})
   });
 })
 
