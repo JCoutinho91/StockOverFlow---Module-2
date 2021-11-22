a81a2b555f7c55463e2387cb71cd09995c83bee7
JosePedro <guedes.jose@gmail.com>
2021-11-22 12:00:03 +0000

    fixed api links

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,10 +1,5 @@
 const router = require("express").Router();
 const axios = require("axios");
-const urlApple = "https://www.styvio.com/apiV2/AAPL/70423544-06fc-489c-82e5-3508e4201962"
-const urlAmazon = "https://www.styvio.com/apiV2/TSLA/70423544-06fc-489c-82e5-3508e4201962"
-const urlTesla = "https://www.styvio.com/apiV2/AMZN/70423544-06fc-489c-82e5-3508e4201962"
-const urlMicrosoft = "https://www.styvio.com/apiV2/MSFT/70423544-06fc-489c-82e5-3508e4201962"
-const urlGoogle = "https://www.styvio.com/apiV2/GOGGL/70423544-06fc-489c-82e5-3508e4201962"
 
 
 router.get("/stocks-views/stock-view", (req,res)=>{
