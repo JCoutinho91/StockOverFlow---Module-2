e1970f419d9dacaf63bfd76fb63225a1d5e323ff
Max Mayer <mxhmayer@gmail.com>
2021-11-23 14:09:49 +0000

    solving merge stock.routes

--- routes/stocks.route.js
+++ routes/stocks.route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const axios = require("axios");
 const Comment = require("./../models/Comment.model");
-const isLoggedIn = require("./../middleware/isLoggedIn")
+const isLoggedIn = require("./../middleware/isLoggedIn");
 require("dotenv").config();
 
 router.get("/stocks-views/stock-view", isLoggedIn, (req, res) => {
@@ -19,21 +19,26 @@ router.get("/stockcategory", isLoggedIn, (req, res) => {
     res.render("./stocks-views/stock-view", { stockList: values });
   });
 });
-router.get("/stock-view-details/:stockId",isLoggedIn, async (req, res) => {
+router.get("/stock-view-details/:stockId", isLoggedIn, async (req, res) => {
   const stockId = req.params.stockId;
-  const userId = req.session.user._id
+  const userId = req.session.user._id;
   try {
     const gettingData = await axios.get(
       `https://www.styvio.com/apiV2/${stockId}/${process.env.API_KEY}`
     );
-    const filteredComments = await Comment.find({ ticker: stockId }).populate("creator");
-    const filteredCommentsCopy = [...filteredComments]
+    const filteredComments = await Comment.find({ ticker: stockId }).populate(
+      "creator"
+    );
+    const filteredCommentsCopy = [...filteredComments];
 
-    const updatedComments = filteredCommentsCopy.map((commentObj)=>{
-     return {...commentObj._doc, isCreator: userId === String(commentObj.creator._id) }
-      })
+    const updatedComments = filteredCommentsCopy.map((commentObj) => {
+      return {
+        ...commentObj._doc,
+        isCreator: userId === String(commentObj.creator._id),
+      };
+    });
 
-     console.log("updatedComments", updatedComments)
+    console.log("updatedComments", updatedComments);
     res.render("./stocks-views/stock-view-details", {
       data: {
         stockInfo: gettingData,
@@ -49,29 +54,24 @@ router.post("/stock-view-details/:stockID/create", isLoggedIn, (req, res) => {
   console.log("in post");
   const stockId = req.params.stockID;
   const { name, comment } = req.body;
-  const userId = req.session.user._id
-  Comment
-  .create({ name: name, comment: comment, ticker: stockId, creator: userId })
-  .then((createdComment) => {
-    }
-  );
+  const userId = req.session.user._id;
+  Comment.create({
+    name: name,
+    comment: comment,
+    ticker: stockId,
+    creator: userId,
+  }).then((createdComment) => {});
   res.redirect(`/stock-view-details/${stockId}`);
 });
 
-router.get("/stock-view-details/:stockID/edit", isLoggedIn, (req,res)=>{
-  const StockId = req.params.stockID
-  const userId = req.session.user._id
-  console.log(userId)
-  console.log()
-  Comment.find({creator: userId})
-  .then((foundComment)=>{
-    res.render(`/stocks-views/${stockId}/comment-edit-view`, { foundComment } );
-  
-
-  })
-})
-  
+router.get("/stock-view-details/:stockID/edit", isLoggedIn, (req, res) => {
+  const StockId = req.params.stockID;
+  const userId = req.session.user._id;
+  console.log(userId);
+  console.log();
+  Comment.find({ creator: userId }).then((foundComment) => {
+    res.render(`/stocks-views/${stockId}/comment-edit-view`, { foundComment });
+  });
+});
 
 module.exports = router;
-
-
