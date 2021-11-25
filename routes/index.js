c3e94fba122594a87e714706e91d132f82292d88
JosePedro <guedes.jose@gmail.com>
2021-11-25 16:56:24 +0000

    fixednavbug

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const axios = require("axios");
 const UserInfo = require("./../models/UserInfo.model");
 const fileUploader = require('./../config/cloudinary.config');
@@ -15,6 +16,52 @@ router.get("/", (req, res, next) => {
 
   res.render("index", {userIsLoggedIn: userIsLoggedIn});
 });
+
+router.post("/",  (req, res) => {
+  let userIsLoggedIn = true;
+  if (req.session.user) {
+    userIsLoggedIn = false;
+  }
+
+  const { username, password } = req.body;
+  
+  const usernameNotProvided = !username || username === "";
+  const passwordNotProvided = !password || password === "";
+
+  if (usernameNotProvided || passwordNotProvided) {
+    res.render("index", {
+      errorMessage: "Provide username and password.",userIsLoggedIn: userIsLoggedIn},
+    );
+    return
+  }
+
+  let user;
+  // Check if the user exists
+  User.findOne({ username: username })
+    .then((foundUser) => {
+      user = foundUser;
+      if (!foundUser) {
+        throw new Error("Wrong credentials");
+      }
+
+      // Compare the passwords
+      return bcrypt.compare(password, foundUser.password);
+    })
+    .then((isCorrectPassword) => {
+      if (!isCorrectPassword) {
+        throw new Error("Wrong credentials");
+      } else if (isCorrectPassword) {
+        req.session.user = user;
+        res.redirect("home-view");
+      }
+    })
+    .catch((err) => {
+      res.render("index", {
+        errorMessage: err.message || "Provide username and password.",
+      });
+    });
+});
+
 
 router.get("/search", isLoggedIn, (req, res) => {
   console.log("req.query", req.query)
@@ -49,13 +96,13 @@ router.get("/Profite-edit/:infoID", isLoggedIn, (req, res) => {
     res.render(`profile-edit`, { user: foundUser });
   });
 });
-
+/*
 pictureFix = () =>{
   if(req.file.path === undefined){
     return url
   }else{req.file.path}
 }
-
+*/
 router.post("/profite-edit/:infoID", fileUploader.single("profile-cover-image"), (req, res) => {
   const Info = req.params.infoID;
   const { firstname, lastname, age, aboutme } = req.body;
