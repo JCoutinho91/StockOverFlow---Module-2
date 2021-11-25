82d1a38b78a732cd3db43b3ce553470909fe78c6
Max Mayer <mxhmayer@gmail.com>
2021-11-25 17:36:00 +0000

    add style

--- models/UserInfo.model.js
+++ models/UserInfo.model.js
@@ -2,13 +2,16 @@ const { Schema, model } = require("mongoose");
 
 const userInfoSchema = new Schema(
   {
-    firstname: { type: String, default: ""},
-    lastname: { type: String, default: ""},
-    age: {type: Number, default: ""},
-    aboutme: {type:String, default: "", maxLength: 400},
-    favoritestocks: {type:String, default: ""},
-    user: { type:String, default: "" },
-    imageUrl: {type: String, default:"/images/default-profile-img.jpg"},
+    firstname: { type: String, default: "" },
+    lastname: { type: String, default: "" },
+    age: { type: Number, default: "" },
+    aboutme: { type: String, default: "", maxLength: 400 },
+    favoritestocks: { type: String, default: "" },
+    user: { type: String, default: "" },
+    imageUrl: {
+      type: String,
+      default: "/images/default-profile-img.jpg",
+    },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
